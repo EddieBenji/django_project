12d0bef4d013dde020006b34e8a504cd1583db3a
Lalo_Canche <eduardo_benji@hotmail.com>
2015-09-02 11:52:14 -0500

    Playing with bookstores app.

--- bookstores/models.py
+++ bookstores/models.py
@@ -8,10 +8,16 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
 
+    def __str__(self):
+        return "Autor: " + self.name + "\n" + "Edad: " + str(self.age)
+
 
 class Publisher(models.Model):
     name = models.CharField(max_length=300)
     num_awards = models.IntegerField()
+
+    def __str__(self):
+        return "Editorial: " + self.name + "\n Número de premios: " + str(self.num_awards)
 
 
 class Book(models.Model):
@@ -19,9 +25,19 @@ class Book(models.Model):
     pages = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.FloatField()
-    authors = models.ManyToManyField(Author)
-    publisher = models.ForeignKey(Publisher)
+    authors = models.ManyToManyField(Author)  # we only need to write this sentence in one place.
+    publisher = models.ForeignKey(Publisher)  # one to one relationship.
+    # publisher = models.OneToOneField(Publisher)  # it's equivalent to the one before, but, when you will
+    # be adding the new element, it won't let you add it until you create the publisher object and you associated.
     pubdate = models.DateField()
+
+    def __str__(self):
+        return "Nombre libro: " + self.name + " \n  " + "Editoriales: " + str(
+            self.publisher) + "Autores: "  # + self.authors.all()
+
+    def my_authors(self):
+        for author in self.authors.all():
+            print(author)
 
 
 class Store(models.Model):
